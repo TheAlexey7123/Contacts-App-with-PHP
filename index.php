95b13faea067bd9f93ea3524accd0b400f72fbfb
alexey <alexeyeuk@gmail.com>
2024-09-13 18:42:13 +0200

    Add dinamic contacts in JSON

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-    $contacts = [
-        ["name" => "alex", "phone" => "677777777"],
-        ["name" => "nate", "phone" => "645654465"],
-        ["name" => "sam", "phone" => "53454354354"],
-        ["name" => "viktor", "phone" => "543543345"],
-        ["name" => "nadine", "phone" => "432423432"]
-    ];
+    // $contacts = [
+    //     ["name" => "alex", "phone" => "677777777"],
+    //     ["name" => "nate", "phone" => "645654465"],
+    //     ["name" => "sam", "phone" => "53454354354"],
+    //     ["name" => "viktor", "phone" => "543543345"],
+    //     ["name" => "nadine", "phone" => "432423432"]
+    // ];
+
+    if(file_exists("contacts.json")){
+        $contacts = json_decode(file_get_contents("contacts.json"), true);
+    }
+
+    else{
+        $contacts = [];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -58,19 +66,29 @@
         <div class="container pt-4 p-3">
             <div class="row">
                 
-            <?php foreach($contacts as $contact){ ?>
+            <?php if(!($contacts)): ?>
+                <div class="col-md-4 mx-auto">
+                    <div class="card card-body text-center">
+                        <p>No contacts saved yet</p>
+                        <a href="./add.php">Add One!</a>
+                    </div>
+                </div>
+            <?php endif ?>
+
+            <?php if(($contacts)): ?>
+            <?php foreach($contacts as $contact): ?>
                 <div class="col-md-4 mb-3">
                     <div class="card text-center">
                         <div class="card-body">
                             <h3 class="card-title text-capitalize"><?= $contact["name"]; ?></h3>
-                            <p class="m-2"><?= $contact["phone"]; ?></p>
+                            <p class="m-2"><?= $contact["phone_number"]; ?></p>
                             <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
                             <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
                         </div>
                     </div>
                 </div>
-            <?php } ?>
-                
+            <?php endforeach ?>
+            <?php endif ?> 
             </div>
         </div>
     </main>
