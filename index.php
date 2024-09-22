882562057810a8d16f2df435475b62ef7c5f5935
alexey <alexeyeuk@gmail.com>
2024-09-22 17:27:20 +0200

    reusing code

--- index.php
+++ index.php
@@ -1,108 +1,48 @@
 <?php
 
-require "database.php";
+    require "database.php";
 
-try{
-    $contacts = $conn->query("select * from contacts");
-}
+    try{
+        $contacts = $conn->query("select * from contacts");
+    }
 
-catch(PDOException $e){
-    print("Error: " . $e->getMessage());
-    die();
-}
-    // $contacts = [
-    //     ["name" => "alex", "phone" => "677777777"],
-    //     ["name" => "nate", "phone" => "645654465"],
-    //     ["name" => "sam", "phone" => "53454354354"],
-    //     ["name" => "viktor", "phone" => "543543345"],
-    //     ["name" => "nadine", "phone" => "432423432"]
-    // ];
+    catch(PDOException $e){
+        print("Error: " . $e->getMessage());
+        die();
+    }
 
-    // if(file_exists("contacts.json")){
-    //     $contacts = json_decode(file_get_contents("contacts.json"), true);
-    // }
-
-    // else{
-    //     $contacts = [];
-    // }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php require "./partials/header.php"; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
-        integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-        crossorigin="anonymous"></script>
-
-    <!-- Static Content -->
-    <link rel="stylesheet" href="./static/css/index.css" />
-
-    <title>Contacts App</title>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand font-weight-bold" href="#">
-                <img class="mr-2" src="./static/img/logo.png" />
-                ContactsApp
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./add.php">Add Contact</a>
-                    </li>
-                </ul>
+<main>
+    <div class="container pt-4 p-3">
+        <div class="row">
+            
+        <?php if($contacts->rowCount() == 0): ?>
+            <div class="col-md-4 mx-auto">
+                <div class="card card-body text-center">
+                    <p>No contacts saved yet</p>
+                    <a href="./add.php">Add One!</a>
+                </div>
             </div>
-        </div>
-    </nav>
+        <?php endif ?>
 
-    <main>
-        <div class="container pt-4 p-3">
-            <div class="row">
-                
-            <?php if(!($contacts)): ?>
-                <div class="col-md-4 mx-auto">
-                    <div class="card card-body text-center">
-                        <p>No contacts saved yet</p>
-                        <a href="./add.php">Add One!</a>
+        <?php if(($contacts)): ?>
+        <?php foreach($contacts as $contact): ?>
+            <div class="col-md-4 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h3 class="card-title text-capitalize"><?= $contact["name"]; ?></h3>
+                        <p class="m-2"><?= $contact["phone_number"]; ?></p>
+                        <a href="edit.php?id=<?= $contact["id"]; ?>" class="btn btn-secondary mb-2">Edit Contact</a>
+                        <a href="delete.php?id=<?= $contact['id']; ?>" class="btn btn-danger mb-2">Delete Contact</a>
                     </div>
                 </div>
-            <?php endif ?>
-
-            <?php if(($contacts)): ?>
-            <?php foreach($contacts as $contact): ?>
-                <div class="col-md-4 mb-3">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h3 class="card-title text-capitalize"><?= $contact["name"]; ?></h3>
-                            <p class="m-2"><?= $contact["phone_number"]; ?></p>
-                            <a href="edit.php?id=<?= $contact["id"]; ?>" class="btn btn-secondary mb-2">Edit Contact</a>
-                            <a href="delete.php?id=<?= $contact['id']; ?>" class="btn btn-danger mb-2">Delete Contact</a>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach ?>
-            <?php endif ?> 
             </div>
+        <?php endforeach ?>
+        <?php endif ?> 
         </div>
-    </main>
-</body>
+    </div>
 
-</html>
+<?php require "./partials/footer.php"; ?>
